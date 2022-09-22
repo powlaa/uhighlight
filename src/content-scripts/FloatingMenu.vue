256f43f219a1d339d32a6c7aa8dd409ec51d50cb
powlaa <s0558576@htw-berlin.de>
2022-09-22 14:41:32 +0200

    Add focus mode icon

--- src/content-scripts/FloatingMenu.vue
+++ src/content-scripts/FloatingMenu.vue
@@ -1,13 +1,19 @@
 <template>
   <div
-    :class="{ 'uhighlight-hidden': hidden, 'uhighlight-floating-menu': true }"
+    :class="{
+      'uhighlight-hidden': floatingMenuHidden,
+      'uhighlight-floating-menu': true,
+    }"
     v-show="visible"
   >
-    <div class="uhighlight-toggle-icon" @click="hidden = !hidden">
-      <ChevronRightIcon v-show="!hidden" />
-      <ChevronLeftIcon v-show="hidden" />
+    <div
+      class="uhighlight-toggle-icon"
+      @click="floatingMenuHidden = !floatingMenuHidden"
+    >
+      <ChevronRightIcon v-show="!floatingMenuHidden" />
+      <ChevronLeftIcon v-show="floatingMenuHidden" />
     </div>
-    <div v-show="!hidden">
+    <div v-show="!floatingMenuHidden">
       <div
         class="uhighlight-dark-mode-icon"
         @click="$emit('darkModeChanged', !darkMode)"
@@ -33,15 +39,35 @@
         </div>
       </div>
       <div class="uhighlight-focus-container">
-        <label class="uhighlight-switch no-highlight">
-          <input
-            type="checkbox"
-            id="focus-mode"
-            v-model="focus"
-            @change="$emit('update:focus', focus)"
-          />
-          <span class="uhighlight-slider round"></span>
-        </label>
+        <div class="uhighlight-focus-switch">
+          <label class="uhighlight-switch no-highlight">
+            <input
+              type="checkbox"
+              id="focus-mode"
+              v-model="focus"
+              @change="$emit('update:focus', focus)"
+            />
+            <span class="uhighlight-slider round"></span>
+          </label>
+          <svg
+            id="instant_highlighter_icon"
+            data-name="instant highlighter icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="18.03"
+            height="16.734"
+            viewBox="0 0 22.03 20.734"
+            style="margin-left: 10px"
+          >
+            <path
+              id="Icon_awesome-highlighter"
+              data-name="Icon awesome-highlighter"
+              d="M0,19.437l4.046,1.3L5.482,19.3,2.767,16.583ZM5.046,9.718a1.482,1.482,0,0,0-.437,1.543l.528,1.734L3.075,15.058l3.9,3.9,2.06-2.06,1.731.53a1.482,1.482,0,0,0,1.545-.437l1.44-1.686L6.728,8.282,5.046,9.718ZM21.378,3.21,18.819.651a2.223,2.223,0,0,0-3.042-.1L7.716,7.438l6.875,6.875,6.882-8.061a2.223,2.223,0,0,0-.1-3.042Z"
+              transform="translate(0 0)"
+              :fill="darkMode ? '#ddd' : '#222'"
+            />
+          </svg>
+        </div>
+
         <div v-show="focus" class="uhighlight-focus-options">
           <ColorChoice
             :colors="colors"
@@ -86,6 +112,7 @@ const emit = defineEmits([
 
 const selectedCategory = ref(null);
 const focus = ref(false);
+const floatingMenuHidden = ref(props.hidden);
 let activeCategories = [];
 let visible = false;
 
@@ -130,10 +157,12 @@ function categoryClicked(evt) {
 .uhighlight-floating-menu,
 .uhighlight-highlights-heading,
 .uhighlight-focus-container,
-.uhighlight-focus-options {
+.uhighlight-focus-options,
+.uhighlight-checkbox-label {
   background-color: var(--uhighlight-background-color-primary) !important;
   color: var(--uhighlight-text-primary-color) !important;
   font-family: var(--uhighlight-font-family) !important;
+  text-transform: none !important;
 }
 .uhighlight-floating-menu {
   position: fixed !important;
@@ -154,6 +183,7 @@ function categoryClicked(evt) {
   height: 60px !important;
   padding: 0px !important;
   border-radius: 10px 0 0 10px !important;
+  pointer-events: auto !important;
 }
 .uhighlight-dark-mode-icon {
   position: absolute !important;
@@ -181,6 +211,9 @@ function categoryClicked(evt) {
   display: inline-block !important;
   margin-top: 10px !important;
   width: 100% !important;
+}
+.uhighlight-focus-switch {
+  display: flex !important;
 }
 .uhighlight-checkbox {
   accent-color: var(--uhighlight-accent-color-secondary) !important;
